03b64eaebc7ac9deac2e730d812ea03308aa9b2f
Leandro de Oliveira Silva <leandrooliveirask8@hotmail.com>
2022-09-13 15:36:46 -0300

    Consulta simples com query methods

--- workshopmongo/src/main/java/com/leandrooliveira/workshopmongo/resources/PostResource.java
+++ workshopmongo/src/main/java/com/leandrooliveira/workshopmongo/resources/PostResource.java
@@ -3,6 +3,7 @@ package com.leandrooliveira.workshopmongo.resources;
 import com.leandrooliveira.workshopmongo.domain.Post;
 import com.leandrooliveira.workshopmongo.domain.User;
 import com.leandrooliveira.workshopmongo.dto.UserDTO;
+import com.leandrooliveira.workshopmongo.resources.util.URL;
 import com.leandrooliveira.workshopmongo.services.PostService;
 import com.leandrooliveira.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,6 +31,17 @@ public class PostResource {
         Post obj = postService.findById(id);
         return ResponseEntity.ok().body(obj);
     }
+
+    @GetMapping(value = "/titlesearch")
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text){
+        text = URL.decodeParam(text);
+        List<Post> list = postService.findByTitle(text);
+
+
+        return ResponseEntity.ok().body(list);
+    }
+
+
 
 
 
