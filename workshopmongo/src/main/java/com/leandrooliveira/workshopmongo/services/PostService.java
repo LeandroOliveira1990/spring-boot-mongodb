6161559defe27a8f71ad5d2675b32f4452ac3340
Leandro de Oliveira Silva <leandrooliveirask8@hotmail.com>
2022-09-13 14:41:28 -0300

    Obtendo um post por id

---
+++ workshopmongo/src/main/java/com/leandrooliveira/workshopmongo/services/PostService.java
@@ -0,0 +1,21 @@
+package com.leandrooliveira.workshopmongo.services;
+
+import com.leandrooliveira.workshopmongo.domain.Post;
+import com.leandrooliveira.workshopmongo.repository.PostRepository;
+import com.leandrooliveira.workshopmongo.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository postRepository;
+
+    public Post findById(String id){
+        Optional<Post> obj = postRepository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+}
